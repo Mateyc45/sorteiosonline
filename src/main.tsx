37ffda281f36dabc59fd7c3236f7d3076b370cd8
Matheus <matheusdosreissilvazavan@gmail.com>
2025-04-21 16:46:44 -0300

    Tentativa de corrigir indexação das páginas

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
+import React from 'react';
 import { StrictMode } from 'react';
+import { HelmetProvider } from 'react-helmet-async';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <HelmetProvider>
     <App />
-  </StrictMode>
+  </HelmetProvider>
 );
